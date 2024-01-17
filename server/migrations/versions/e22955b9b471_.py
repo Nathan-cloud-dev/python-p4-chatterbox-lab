22fe46fe9e7480849749c09c90362ef355b9565a
Nathan-cloud-dev <kiprotichnathan@yahoo.com>
2024-01-17 11:21:14 +0300

    done

---
+++ server/migrations/versions/e22955b9b471_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: e22955b9b471
+Revises: 
+Create Date: 2024-01-17 11:07:40.954480
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e22955b9b471'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
